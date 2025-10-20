40aed0f1a1113ed2add55cfadaf69f41b83febe3
Ryan McCoy <ryanphilipmccoy@gmail.com>
2025-10-20 14:22:15 -0700

    Make minor changes to errors and restrict to GET requests

--- index.js
+++ index.js
@@ -6,15 +6,19 @@ const port = 3000
 const cwd = process.cwd()
 
 app.use((req, res, next) => {
+    if (req.method !== "GET") {
+        res.status(418).send("418 I'm A Teapot. The server is only capable of handelling GET requests.")
+        return
+    }
     let url = req.url
     console.log(`Request recieved at ${url}`)
     if (url.includes('..')) {
-        res.status(400).send("Fuck you")
+        res.status(403).send("403 Forbidden")
         return
     }
     fs.readFile(`${cwd}${url}`, 'utf-8', (err, data) => {
         if (err) {
-            res.status(500).send('404')
+            res.status(404).send('404 Not Found')
         }
         res.status(200).send(data)
     })
